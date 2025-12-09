bf74c9c879cd470ae7f377f4b7531303167e96ef
Fadhel <fadhelmr1710@gmail.com>
2025-12-09 10:22:54 +0700

    update to almoust done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const marquee = document.getElementById("marquee");
 const text = ` |  Instagram: https://www.instagram.com/fadhelmr123/  |  Gmail: fadhelmr1710@gmail.com  |  Linkedin: https://www.linkedin.com/in/fadhel-muhammad-ramadhan-568418376/  |  Github: https://github.com/FadhelMR  `;
 
-marquee.innerHTML = `<span>${text.repeat(12)}</span>`;
+marquee.innerHTML = `<span>${text.repeat(5)}</span>`;
 
 window.addEventListener("scroll", () => {
   let scrollTop = document.documentElement.scrollTop;
@@ -13,3 +13,52 @@ window.addEventListener("scroll", () => {
 
   document.getElementById("scroll-indicator").style.width = scrolled + "%";
 });
+
+// Detect Section Notification
+const notif = document.getElementById("sectionNotif");
+let lastSection = "";
+
+function showNotif(text) {
+  notif.textContent = text;
+  notif.classList.add("show");
+
+  setTimeout(() => {
+    notif.classList.remove("show");
+  }, 2400); // muncul 2.4 detik
+}
+
+// Detect section on scroll
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+
+  sections.forEach((sec) => {
+    const rect = sec.getBoundingClientRect();
+
+    if (
+      rect.top <= window.innerHeight / 2 &&
+      rect.bottom >= window.innerHeight / 2
+    ) {
+      const current = sec.getAttribute("data-title");
+
+      if (current && current !== lastSection) {
+        showNotif(current);
+        lastSection = current;
+      }
+    }
+  });
+});
+
+// Notif saat pertama kali masuk website
+window.addEventListener("load", () => {
+  showNotif("Hi, welcome to my website");
+});
+
+function showAlert(type, message) {
+  const notif = document.getElementById("notif");
+  notif.className = "notif " + type + " show";
+  notif.innerText = message;
+
+  setTimeout(() => {
+    notif.classList.remove("show");
+  }, 3000);
+}
